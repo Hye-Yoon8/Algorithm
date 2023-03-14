dd70cc874231de006c6ebbdbc8ff1d3f2f508856
Hye-Yoon8 <dwon8400@naver.com>
2023-03-14 21:23:14 +0900

    # 1149

---
+++ 1149.py
@@ -0,0 +1,16 @@
+# 백준 1149번 - RGB거리
+
+import sys
+
+N = int(sys.stdin.readline())
+RGB = []
+
+for i in range(N):
+    RGB.append(list(map(int, sys.stdin.readline().split())))
+
+for i in range(1, N):
+    RGB[i][0] = min(RGB[i - 1][1], RGB[i - 1][2]) + RGB[i][0]
+    RGB[i][1] = min(RGB[i - 1][0], RGB[i - 1][2]) + RGB[i][1]
+    RGB[i][2] = min(RGB[i - 1][0], RGB[i - 1][1]) + RGB[i][2]
+
+print(min(RGB[N - 1][0], RGB[N - 1][1], RGB[N - 1][2]))
