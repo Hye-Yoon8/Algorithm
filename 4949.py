f3a3eb67e997d40aca622f804f66e02ac7b33997
Hye-Yoon8 <dwon8400@naver.com>
2023-03-27 22:11:22 +0900

    # 4949

---
+++ 4949.py
@@ -0,0 +1,31 @@
+# 백준 4949번 - 균형잡힌 세상
+
+while(True):
+    string = input()
+    stack = []
+
+    if string == '.':
+        break
+
+    for i in string :
+        if i == '[' or i == '(' :
+            stack.append(i)
+        
+        elif i == ']' :
+            if len(stack) != 0 and stack[-1] == '[' :
+                stack.pop()
+            else : 
+                stack.append(']')
+                break
+        
+        elif i == ')' :
+            if len(stack) != 0 and stack[-1] == '(' :
+                stack.pop()
+            else :
+                stack.append(')')
+                break
+    
+    if len(stack) == 0 :
+        print('yes')
+    else :
+        print('no')
