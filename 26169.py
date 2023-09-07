782fe4d0e400fc2349e28fa1c3cec32d6898c47e
Hye-Yoon8 <dwon8400@naver.com>
2023-09-08 02:31:51 +0900

    # 26169

--- 26169.py
+++ 26169.py
@@ -4,7 +4,7 @@
 import sys
 input = sys.stdin.readline
 
-def DFS(r, c, board, depth, APPLE):
+def DFS(r, c, depth, APPLE):
     global isMoreThan2
     
     visited[r][c] = True
@@ -23,32 +23,35 @@ def DFS(r, c, board, depth, APPLE):
         return
 
     # up
-    if ((r > 0) and (visited[r-1][c] != True) and (board[r-1][c] != -1)):
-        if board[r-1][c] == 1:
-            DFS(r-1, c, board, currentDepth + 1, currentAPPLE + 1)
+    if ((r > 0) and (visited[r-1][c] != True) and (BOARD[r-1][c] != -1)):
+        if BOARD[r-1][c] == 1:
+            DFS(r-1, c, currentDepth + 1, currentAPPLE + 1)
         else:
-            DFS(r-1, c, board, currentDepth + 1, currentAPPLE)
+            DFS(r-1, c, currentDepth + 1, currentAPPLE)
     
     # down
-    if ((r < 4) and (visited[r+1][c] != True) and (board[r+1][c] != -1)):
-        if board[r+1][c] == 1:
-            DFS(r+1, c, board, currentDepth + 1, currentAPPLE + 1)
+    if ((r < 4) and (visited[r+1][c] != True) and (BOARD[r+1][c] != -1)):
+        if BOARD[r+1][c] == 1:
+            DFS(r+1, c, currentDepth + 1, currentAPPLE + 1)
         else:
-            DFS(r+1, c, board, currentDepth + 1, currentAPPLE)
+            DFS(r+1, c, currentDepth + 1, currentAPPLE)
     
     # left
-    if ((c > 0) and (visited[r][c-1] != True) and (board[r][c-1] != -1)):
-        if board[r][c-1] == 1:
-            DFS(r, c-1, board, currentDepth + 1, currentAPPLE + 1)
+    if ((c > 0) and (visited[r][c-1] != True) and (BOARD[r][c-1] != -1)):
+        if BOARD[r][c-1] == 1:
+            DFS(r, c-1, currentDepth + 1, currentAPPLE + 1)
         else:
-            DFS(r, c-1, board, currentDepth + 1, currentAPPLE)
+            DFS(r, c-1, currentDepth + 1, currentAPPLE)
     
     # right
-    if ((c < 4) and (visited[r][c+1] != True) and (board[r][c+1] != -1)):
-        if board[r][c+1] == 1:
-            DFS(r, c+1, board, currentDepth + 1, currentAPPLE + 1)
+    if ((c < 4) and (visited[r][c+1] != True) and (BOARD[r][c+1] != -1)):
+        if BOARD[r][c+1] == 1:
+            DFS(r, c+1, currentDepth + 1, currentAPPLE + 1)
         else:
-            DFS(r, c+1, board, currentDepth + 1, currentAPPLE)
+            DFS(r, c+1, currentDepth + 1, currentAPPLE)
+    
+    # 다른 경로와 해당 노드를 공유하는 경우를 위해 추가 : 백트래킹
+    visited[r][c] = False
             
     if currentDepth == 0:
         if isMoreThan2 == True:
@@ -57,13 +60,10 @@ def DFS(r, c, board, depth, APPLE):
             print(0)
 
 
-BOARD = []
-for _ in range(5):
-    BOARD.append(list(map(int, input().split())))
-
+BOARD = [list(map(int, input().split())) for _ in range(5)]
 r, c = map(int, input().split())
 
 visited = [[False for _ in range(5)] for _ in range(5)]
 isMoreThan2 = False
 
-DFS(r, c, BOARD, 0, 0)
+DFS(r, c, 0, 0)
